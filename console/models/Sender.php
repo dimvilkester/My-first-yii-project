b4ec80342f6851f38968ddd9d7cdb428be2f5d5f
Dmitry Vilkin <kiddr@rambler.ru>
2017-08-10 22:12:39 +0300

    Lesson 10.3(b)

--- console/models/Sender.php
+++ console/models/Sender.php
@@ -22,22 +22,43 @@ class Sender
     
     public static function running($associateList){
         
-        $timeStamp = Yii::$app->formatter->asDate('now', 'php:d.m.Y') ."\n";
+        $data = Yii::$app->formatter->asDate('now', 'php:d.m.Y') ."\n";
         
         foreach ($associateList as $associate) {
             $result = Yii::$app->mailer->compose('/mailer/associate', [
                 'associate' => $associate,
-                'timeStamp' => $timeStamp,
+                'data' => $data,
                 
             ])
                 ->setFrom('kiddrtest@gmail.com')
                 ->setTo($associate['email'])
                 ->setSubject('Начисление зарплаты')
-                ->send();
-            
-            var_dump($result);
+                ->send();         
         }
 
-    }
+        self::log();
 
+    }
+    
+    public static function log() {
+        
+        Yii::$app->formatter->locale = 'ru-RU';
+        $timeStamp = Yii::$app->formatter->asDate('now') .' Ежемесячной зачислении заработной платы бригаде №1' ."\n";
+        
+        $fileName = '/var/www/project/frontend/web/log.txt';
+
+        if(!$fileName) {
+            echo 'Ошибка открытия файла';
+        } else {
+            // Открываем текстовый файл для записи
+            $logTime = fopen($fileName, 'ab') or die('Не удалось создать файл');
+
+            // Записываем текущую дату
+            fwrite($logTime, $timeStamp); 
+
+            // Закрываем текстовый файл
+            fclose($logTime);
+        }
+    }
+    
 }
